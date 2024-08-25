2be21e68cb53849fb5082a9da27fcaddfdacdf3e
Michael <michaelharlow1@outlook.com>
2024-08-24 19:10:21 -0500

    feat: init commit

---
+++ init.lua
@@ -0,0 +1,23 @@
+require("dev")
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.o.nu = true
+vim.o.relativenumber = true
+
+vim.o.smartindent = true
+
+vim.o.wrap = false
+
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+vim.o.scrolloff = 8
+
+
+
+
